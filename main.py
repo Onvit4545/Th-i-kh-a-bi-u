be0f2ee20ecc146f97eb22a496f5cc9611d0a00b
Onvit4545 <156510676+Onvit4545@users.noreply.github.com>
2025-06-16 19:42:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
-import pandas as pd
-import numpy as np
-import random
-detail_class = np.loadtxt('data\detail_class.csv', dtype = str, delimiter=',')
-lop =  ' '.join(i[0] for i in detail_class).split()
-monhoc = ' '.join(i[1] for i in detail_class).split()
-s = pd.Series(monhoc, index=lop)
-default_sllop =pd.Series([4,4,4,2,2,2,2,2,2,2], index=[1,2,3,4,5,6,7,8,9,10])
-mang=[]
-mang_kq2 = []
+from datetime import datetime
 
-for i in range(len(s)):
-    
-    for ij in list(s[i]):
-        for j in range(default_sllop[int(ij)]):
-            mang.append(int(ij))
-    mang_kq1 = []
-    for i in range(len(mang)):
-        sdc = random.choice(mang)
-        mang_kq1.append(sdc)
-        mang.remove(sdc)
-    mang_kq2.append(mang_kq1)
-print(mang_kq2)
+class Event:
+    def __init__(self,title,start_time,end_time):
+        
+        self.title = title
+        self.start_time = start_time
+        self.end_time = end_time
+    def duration(self):
+        duration = self.end_time-self.start_time
+        return duration
+    def notify(self):
+        if datetime.now() < self.start_time:
+            return 'Sắp bắt đầu'
+        elif datetime.now() > self.end_time:
+            return 'Hết Hạn'
+        else:
+            return 'Đang diễn ra'
+class Lich:     
+    def __init__(self):
+        self.event_list=[]
+    def add_event(self,event):
+        if self.event_list ==[]:
+                self.event_list.append(event)
+        else:
+            for i in self.event_list:         
+                if i.start_time <= event.end_time or i.end_time <= event.start_time:
+                    print(f'Sự kiện {event.title} bị trùng')
+                    continue
+                    
+                else:
+                    self.event_list.append(event)
 
+    def show_event(self):
+        return self.event_list
 
-    
+lich=Lich()
+event1 = Event('gi1do',datetime(2025, 6, 12, 14, 30),datetime(2025, 6, 16, 15, 20))
+event2 = Event('gi2do',datetime(2025, 6, 12, 14, 30),datetime(2025, 6, 16, 15, 20))
+
+lich.add_event(event1)
+lich.add_event(event2)
+for i in lich.show_event():  
+    print(i.notify())
